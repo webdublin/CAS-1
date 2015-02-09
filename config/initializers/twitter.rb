6d831155833815b4b334f120b220c5fbe59d52a4
Patrick Comerford <patrickcomerford@Patricks-MacBook-Pro.local>
2015-02-09 21:17:15 +0000

    added new files

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key = "P5hiTTcw6ICdZT80QfvBHgYT0"
+  config.consumer_secret = "42WeIu3IwVhfLmNvEPl9gVIzIIvqExJqzAtDVcKMWn3zdYepGA"
+  config.access_token = "21917926-aKo1J6gQl8VdUxwhO46zsdhegTGSCJoVnTneFBc9Q"
+  config.access_token_secret = "ShqDYaZSLao4cRNte7ftQHCLDd6bd4aQZvuWXIEw"
+end
